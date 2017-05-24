a5e3568d3c766d6095716b1c34c475d5f0b47f8b
Riquelvi Contreras <riquelvicontreras@hotmail.com>
2017-05-24 21:55:21 +0000

    Updated contact controller, re-done contact mailer file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,15 +1,11 @@
 class ContactMailer < ActionMailer::Base
     default to: 'riquelvicontreras@gmail.com'
     
-    def contact_mail(name, email, body)
+    def contact_email(name, email, body)
         @name = name
         @email = email 
         @body = body
         
         mail(from: email, subject: 'Contact Form Message')
     end
-end  
-
-
-    
-        
+end 
