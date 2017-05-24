5651a45a24a5a9a9980ae6043625f348ec38c60f
Riquelvi Contreras <riquelvicontreras@hotmail.com>
2017-05-24 23:30:28 +0000

    Updated mailer settings environment file

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,16 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_setting = { 
-    :port           => ENV['MAILGUN_STMP_PORT'],
-    :address        => ENV['MAILGUN_STMP_SERVER'],
-    :user_domain    => ENV['MAILGUN_STMP_LOGIN'],
-    :password       => ENV['MAILGUN_STMP_PASSWORD'],
-    :domain         => 'secret-oasis-85930.herokuapp.com',
-    :authentication => :plain,
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'secret-oasis-85930.herokuapp.com',
+  :authentication => :plain,
 }
 
-ActionMailer::Base.delivery_method = :stmp
+ActionMailer::Base.delivery_method = :smtp
+
+
 
