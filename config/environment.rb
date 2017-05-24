a454e76d362efd616319348c9186be427aed051f
Riquelvi Contreras <riquelvicontreras@hotmail.com>
2017-05-24 23:09:31 +0000

    Added mail settings to  environment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_setting = { 
+    :port           => ENV['MAILGUN_STMP_PORT'],
+    :address        => ENV['MAILGUN_STMP_SERVER'],
+    :user_domain    => ENV['MAILGUN_STMP_LOGIN'],
+    :password       => ENV['MAILGUN_STMP_PASSWORD'],
+    :domain         => 'secret-oasis-85930.herokuapp.com',
+    :authentication => :plain,
+}
+
+ActionMailer::Base.delivery_method = :stmp
+
